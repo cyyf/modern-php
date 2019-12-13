f5457ea20eda3c8b2a6f020248887570cd26ce27
yyy <834702600@qq.com>
2019-12-13 18:02:23 +0800

    测试composer

---
+++ src/Scanner.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace cy\modern\url;
+
+use GuzzleHttp\Client;
+use League\Csv\Exception;
+
+class Scanner
+{
+    protected $urls;
+
+    protected $httpClient;
+
+    public function __construct(array $urls)
+    {
+        $this->urls = $urls;
+        $this->httpClient = new Client();
+    }
+
+    public function getInvalidUrls(){
+        $invalidUrls = [];
+        foreach ($this->urls as $url){
+            try {
+                $response = $this->httpClient->get($url);
+                $statusCode = $response->getStatusCode();
+            }catch (Exception $exception){
+                $statusCode = 500;
+            }
+
+            if($statusCode >= 400){
+                $invalidUrls[] = [
+                    'url' => $url,
+                    'status' => $response->getStatusCode()
+                ];
+            }
+        }
+
+        return $invalidUrls;
+    }
+}
